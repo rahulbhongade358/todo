9ca743e32bb53807feac889833b15a4aa1fcfe5e
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-04-13 19:35:55 +0530

    added routing

--- todo/src/components/Show/Show.jsx
+++ todo/src/components/Show/Show.jsx
@@ -5,9 +5,9 @@ function Show() {
   return (
     <div>
       <h1>Show Note</h1>
-      <div className="fixed-buttons">
+    <div className="fixed-buttons">
        <NavLink to={"/add"}><button className="mission-button">Add</button></NavLink>
-            <NavLink to={"/"}><button className="mission-button"> Home</button></NavLink>
+       <NavLink to={"/"}><button className="mission-button"> Home</button></NavLink>
     </div>
     </div>
   )
